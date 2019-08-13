2c6a4c9df7cf238bcb95b53a73b81f270429da39
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-12 23:14:43 -0400

    Person Class set up - lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,9 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(bio){
+        this.name = bio.name;
+        this.age = bio.age;
+        this.location = bio.location;
+        this.gender = bio.gender;
+    }
+}
